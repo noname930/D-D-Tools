148180ce6a1daf5869212d4ec10eb2c7d1dca8f1
noname930 <alessiospina93@hotmail.it>
2017-11-15 14:19:17 +0100

    v0.1

--- DnD Tools/src/dndtools/DnDTools.java
+++ DnD Tools/src/dndtools/DnDTools.java
@@ -5,6 +5,10 @@
  */
 package dndtools;
 
+import static java.lang.Integer.max;
+import static java.lang.Integer.min;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  *
  * @author Utente
@@ -12,12 +16,69 @@ package dndtools;
 public class DnDTools {
 
     /**
-     * @param args the command line arguments
+     * @param numero_item
      */
+    
+    protected static void randomItem(int numero_item)
+    {
+         int i;
+         int randomNum;
+     
+         for (i=0; i<numero_item; i++)
+         {
+             randomNum=ThreadLocalRandom.current().nextInt(0, 100 + 1);
+             
+             if(randomNum >= 0 && randomNum <= 59) // 60%
+             {
+                if(guiDDtools.jCheckBox1.isSelected())
+                    System.out.println("RARITA 1" + " : " + randomNum);
+                else i--;
+             }
+             else if(randomNum >= 60 && randomNum <=74) // 15%
+             {
+                  if(guiDDtools.jCheckBox2.isSelected())
+                    System.out.println("RARITA 2"+ " : " + randomNum);
+                  else i--;
+             }
+             else if(randomNum >= 75 && randomNum <= 84) // 10%
+             {
+                 if(guiDDtools.jCheckBox3.isSelected())
+                    System.out.println("RARITA 3"+ " : " + randomNum);
+                 else
+                     i--;
+             }
+             else if(randomNum >= 85 && randomNum <= 92) // 7%
+             {
+                  if(guiDDtools.jCheckBox4.isSelected())
+                    System.out.println("RARITA 4"+ " : " + randomNum);
+                  else i--;
+             }
+             else if(randomNum >= 93 && randomNum <= 97 ) // 5%
+             {
+                 if(guiDDtools.jCheckBox5.isSelected())
+                    System.out.println("RARITA 5"+ " : " + randomNum);
+                 else
+                     i--;
+             }
+             else // 3%
+             {  
+                  if(guiDDtools.jCheckBox6.isSelected())
+                    System.out.println("RARITA 6"+ " : " + randomNum);
+                  else
+                      i--;
+             }
+         }
+    }
+    
+    protected static void readExcel(int indice)
+    {
+        
+    }
     public static void main(String[] args) {
         // TODO code application logic here
        
-        
+        guiDDtools main= new guiDDtools();
+        main.setVisible(true);
     }
     
 }
